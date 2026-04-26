589dddeec325d2e19e52e5f68e06fc882e493a59
brdalton <brdalton007@gmail.com>
2026-04-25 18:45:01 -0600

    Update supabaseClient.js

--- src/shared/supabaseClient.js
+++ src/shared/supabaseClient.js
@@ -9,11 +9,18 @@ export async function login() {
   const email = prompt("Enter email for magic link login:");
   if (!email) return;
 
+  //detect environment
+  const isGithub = location.hostname === "brdalton.github.io";
+  const redirectUrl = isGithub
+    ? "https://brdalton.github.io/FamilyQuestEditor/index.html"
+    : "http://127.0.0.1:5500/index.html";
+
   const { error } = await supabase.auth.signInWithOtp({
     email,
     options: {
+      emailRedirectTo: redirectUrl
       //emailRedirectTo: 'http://127.0.0.1:5500/index.html'
-	    emailRedirectTo: 'https://brdalton.github.io/FamilyQuestEditor/index.html'
+	    //emailRedirectTo: 'https://brdalton.github.io/FamilyQuestEditor/index.html'
     }
   });
 
